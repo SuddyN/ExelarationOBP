d589678ddf9bc2a2ae2accb0eef1293dc09adc44
SuddyN <suddy121800@hotmail.com>
2022-06-10 12:09:44 -0400

    format program.cs

--- src/JavaScriptClient/Program.cs
+++ src/JavaScriptClient/Program.cs
@@ -1,7 +1,7 @@
-using System.IdentityModel.Tokens.Jwt;
-using System.Security.Claims;
 using Duende.Bff.Yarp;
 using Microsoft.AspNetCore.Authorization;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,15 +13,13 @@ builder.Services
     .AddRemoteApis();
 
 builder.Services
-    .AddAuthentication(options =>
-    {
+    .AddAuthentication(options => {
         options.DefaultScheme = "Cookies";
         options.DefaultChallengeScheme = "oidc";
         options.DefaultSignOutScheme = "oidc";
     })
     .AddCookie("Cookies")
-    .AddOpenIdConnect("oidc", options =>
-    {
+    .AddOpenIdConnect("oidc", options => {
         options.Authority = "https://localhost:5001";
         options.ClientId = "bff";
         options.ClientSecret = "secret";
@@ -32,8 +30,7 @@ builder.Services
     });
 
 var app = builder.Build();
-if (app.Environment.IsDevelopment())
-{
+if (app.Environment.IsDevelopment()) {
     app.UseDeveloperExceptionPage();
 }
 
@@ -47,8 +44,7 @@ app.UseBff();
 
 app.UseAuthorization();
 
-app.UseEndpoints(endpoints =>
-{
+app.UseEndpoints(endpoints => {
     endpoints.MapBffManagementEndpoints();
 
     // Uncomment this for Controller support
@@ -64,9 +60,8 @@ app.UseEndpoints(endpoints =>
 
 app.Run();
 
-[Authorize] 
-static IResult LocalIdentityHandler(ClaimsPrincipal user, HttpContext context)
-{
+[Authorize]
+static IResult LocalIdentityHandler(ClaimsPrincipal user, HttpContext context) {
     var name = user.FindFirst("name")?.Value ?? user.FindFirst("sub")?.Value;
     return Results.Json(new { message = "Local API Success!", user = name });
 }
