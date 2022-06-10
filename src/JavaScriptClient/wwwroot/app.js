df86cd44c7d555fb57b535a06638a2fd096e7da4
SuddyN <suddy121800@hotmail.com>
2022-06-10 11:33:31 -0400

    format

--- src/JavaScriptClient/wwwroot/app.js
+++ src/JavaScriptClient/wwwroot/app.js
@@ -1,39 +1,39 @@
 function log() {
-  document.getElementById("results").innerText = "";
+    document.getElementById("results").innerText = "";
 
-  Array.prototype.forEach.call(arguments, function (msg) {
-    if (typeof msg !== "undefined") {
-      if (msg instanceof Error) {
-        msg = "Error: " + msg.message;
-      } else if (typeof msg !== "string") {
-        msg = JSON.stringify(msg, null, 2);
-      }
-      document.getElementById("results").innerText += msg + "\r\n";
-    }
-  });
+    Array.prototype.forEach.call(arguments, function (msg) {
+        if (typeof msg !== "undefined") {
+            if (msg instanceof Error) {
+                msg = "Error: " + msg.message;
+            } else if (typeof msg !== "string") {
+                msg = JSON.stringify(msg, null, 2);
+            }
+            document.getElementById("results").innerText += msg + "\r\n";
+        }
+    });
 }
 
 let userClaims = null;
 
 (async function () {
-  var req = new Request("/bff/user", {
-    headers: new Headers({
-      "X-CSRF": "1",
-    }),
-  });
+    var req = new Request("/bff/user", {
+        headers: new Headers({
+            "X-CSRF": "1",
+        }),
+    });
 
-  try {
-    var resp = await fetch(req);
-    if (resp.ok) {
-      userClaims = await resp.json();
+    try {
+        var resp = await fetch(req);
+        if (resp.ok) {
+            userClaims = await resp.json();
 
-      log("user logged in", userClaims);
-    } else if (resp.status === 401) {
-      log("user not logged in");
+            log("user logged in", userClaims);
+        } else if (resp.status === 401) {
+            log("user not logged in");
+        }
+    } catch (e) {
+        log("error checking user status");
     }
-  } catch (e) {
-    log("error checking user status");
-  }
 })();
 
 document.getElementById("login").addEventListener("click", login, false);
@@ -42,56 +42,56 @@ document.getElementById("remote").addEventListener("click", remoteApi, false);
 document.getElementById("logout").addEventListener("click", logout, false);
 
 function login() {
-  window.location = "/bff/login";
+    window.location = "/bff/login";
 }
 
 function logout() {
-  if (userClaims) {
-    var logoutUrl = userClaims.find(
-      (claim) => claim.type === "bff:logout_url"
-    ).value;
-    window.location = logoutUrl;
-  } else {
-    window.location = "/bff/logout";
-  }
+    if (userClaims) {
+        var logoutUrl = userClaims.find(
+            (claim) => claim.type === "bff:logout_url"
+        ).value;
+        window.location = logoutUrl;
+    } else {
+        window.location = "/bff/logout";
+    }
 }
 
 async function localApi() {
-  var req = new Request("/local/identity", {
-    headers: new Headers({
-      "X-CSRF": "1",
-    }),
-  });
+    var req = new Request("/local/identity", {
+        headers: new Headers({
+            "X-CSRF": "1",
+        }),
+    });
 
-  try {
-    var resp = await fetch(req);
+    try {
+        var resp = await fetch(req);
 
-    let data;
-    if (resp.ok) {
-      data = await resp.json();
+        let data;
+        if (resp.ok) {
+            data = await resp.json();
+        }
+        log("Local API Result: " + resp.status, data);
+    } catch (e) {
+        log("error calling local API");
     }
-    log("Local API Result: " + resp.status, data);
-  } catch (e) {
-    log("error calling local API");
-  }
 }
 
 async function remoteApi() {
-  var req = new Request("/remote/identity", {
-    headers: new Headers({
-      "X-CSRF": "1",
-    }),
-  });
+    var req = new Request("/remote/identity", {
+        headers: new Headers({
+            "X-CSRF": "1",
+        }),
+    });
 
-  try {
-    var resp = await fetch(req);
+    try {
+        var resp = await fetch(req);
 
-    let data;
-    if (resp.ok) {
-      data = await resp.json();
+        let data;
+        if (resp.ok) {
+            data = await resp.json();
+        }
+        log("Remote API Result: " + resp.status, data);
+    } catch (e) {
+        log("error calling remote API");
     }
-    log("Remote API Result: " + resp.status, data);
-  } catch (e) {
-    log("error calling remote API");
-  }
 }
