46ac78875556592f73fb34ef0afa9f02d7ec5be8
SuddyN <suddy121800@hotmail.com>
2022-06-10 10:24:53 -0400

    Register the database context

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,14 +1,18 @@
 // Copyright (c) Duende Software. All rights reserved.
 // See LICENSE in the project root for license information.
 
+using ExelarationOBPAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+// Register DB context
+builder.Services.AddDbContext<CountryStateContext>(opt => opt.UseInMemoryDatabase("CountryState"));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 builder.Services.AddAuthentication("Bearer")
     .AddJwtBearer(options => {
         options.Authority = "https://localhost:5001";
@@ -25,8 +29,6 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment()) {
-    app.UseSwagger();
-    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
